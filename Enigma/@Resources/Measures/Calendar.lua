0d6409830e45dcbb18206486341aad43edb4ef65
John Murphy <smurfier20@gmail.com>
2012-07-01 17:37:42 -0500

    Changed Calendar to explicitly set Meter Styles.
    Also, some code cleanup.

--- Enigma/@Resources/Measures/Calendar.lua
+++ Enigma/@Resources/Measures/Calendar.lua
@@ -2,18 +2,13 @@ function Initialize()
 	sRange = string.lower(SELF:GetOption('Range','month'))
 	iStartOnMondays = SELF:GetNumberOption('StartOnMonday',0)>0
 	iLeadingZeroes = SELF:GetNumberOption('LeadingZeroes',0)>0
-	iEDaysColor = SELF:GetNumberOption('ExtraDays')>0 and ' | StyleCalendarTextExtra' or ' | StyleCalendarTextInvisible'
+	iEDaysColor ='StyleCalendarText'..(SELF:GetNumberOption('ExtraDays')>0 and 'Extra' or 'Invisible')
 	
 	iRange = {month=42,week=7}
 	if not iRange[sRange] then sRange='month' end
 	tCurrMonth = {31,28,31,30,31,30,31,31,30,31,30,31}
 	iDayOnLastUpdate=0
-	
-	tMeterStyles = {}
-	for a = 1, iRange[sRange] do
-		local mSkin = SKIN:GetMeter('Day'..a)
-		tMeterStyles[a] = mSkin:GetOption('MeterStyle')
-	end
+
 	local tLabels = {'S','M','T','W','R','F','S'}
 	for i=1,7 do
 		SKIN:Bang('!SetOption','Day'..i..'Label','Text',tLabels[iStartOnMondays and i%7+1 or i])
@@ -25,30 +20,30 @@ function Update()
 	if Date.day ~= iDayOnLastUpdate then
 		iDayOnLastUpdate=Date.day
 		tCurrMonth[2] = 28+(((Date.year%4==0 and Date.year%100~=0) or Date.year%400==0) and 1 or 0)
-		local iStartDay = Rotate(tonumber(os.date('%w', os.time({year=Date.year, month=Date.month, day=1}))))
+		local iStartDay = Rotate(tonumber(os.date('%w', os.time{year=Date.year, month=Date.month, day=1})))
 		
 		----------------------------------------------
 		-- !SETOPTIONS
-		
 		
 		local case={
 			week=function(z) return Date.day+((z-1)-Rotate(Date.wday-1)) end,
 			month=function(z) return z-iStartDay end,
 		}
 		for a = 1, iRange[sRange]  do
-			mStyle=''
+			styles={'StyleCalendarText'}
+			if a%7==1 then table.insert(styles,'StyleCalendar'..(a==1 and 'TextFirst' or 'NewWeek')) end
 			b=case[sRange](a)
 			if b<1 then
 				b=b+tCurrMonth[Date.month==1 and 12 or Date.month-1 ]
-				mStyle=iEDaysColor
+				table.insert(styles,iEDaysColor)
 			elseif b>tCurrMonth[Date.month] then
 				b=b-tCurrMonth[Date.month]
-				mStyle=iEDaysColor
+				table.insert(styles,iEDaysColor)
 			end
-			for k,v in pairs({
-				MeterStyle=tMeterStyles[a]..mStyle,
+			for k,v in pairs{
+				MeterStyle=table.concat(styles,'|'),
 				Text=iLeadingZeroes and string.format('%02d',b) or b,
-			}) do SKIN:Bang('!SetOption','Day'..a,k,v) end
+			} do SKIN:Bang('!SetOption','Day'..a,k,v) end
 		end
 		if sRange == 'month' then
 			SKIN:Bang('!SetVariable','ThisWeek',math.ceil((Date.day+iStartDay)/7))
