3509dd9823e7c93aeed62e3d8c5ca7a49968615d
Michael Engard <Kaelri@gmail.com>
2012-07-11 13:44:15 -0400

    Styles: changed skin width calculation method.

---
+++ Enigma/@Resources/Styles/Enigma Dark/TaskbarSkinWidth.lua
@@ -0,0 +1,44 @@
+function Initialize()
+	LastW = nil
+	MeterText = SKIN:GetMeter('Text')
+	MeterLabel = SKIN:GetMeter('Label')
+	MinW = SELF:GetNumberOption('MinWidth', 0)
+	MaxW = SELF:GetNumberOption('MaxWidth', SKIN:GetVariable('WORKAREAWIDTH'))
+	Variant = SKIN:GetVariable('Variant')
+end
+
+function Update()
+	if Variant ~= 'Textless' then
+		TextW = MeterText:GetW()
+		LabelW = MeterLabel:GetW()
+		if Variant == 'Mini' or Variant == 'Tiny' then
+			W = TextW + LabelW
+		else
+			W = math.max(TextW, LabelW)
+		end
+		W = math.min(math.max(W, MinW), MaxW)
+	else
+		W = MinW
+	end
+
+	if W ~= LastW then
+		SKIN:Bang('!SetVariable', 'TaskbarSkinWidth', W)
+		if Variant == 'Mini' or Variant == 'Tiny' then
+			SKIN:Bang('!MoveMeter', 5 + LabelW, MeterLabel:GetY(), 'Text')
+			if TextW > MaxW - LabelW then
+				SKIN:Bang('!SetOption', 'Text', 'ClipString', 1)
+				SKIN:Bang('!SetOption', 'Text', 'W', MaxW - LabelW)
+			end
+		else
+			if TextW > MaxW then
+				SKIN:Bang('!SetOption', 'Text', 'ClipString', 1)
+				SKIN:Bang('!SetOption', 'Text', 'W', MaxW)
+			end
+			if LabelW > MaxW then
+				SKIN:Bang('!SetOption', 'Label', 'ClipString', 1)
+				SKIN:Bang('!SetOption', 'Label', 'W', MaxW)
+			end
+		end
+		LastW = W
+	end
+end
