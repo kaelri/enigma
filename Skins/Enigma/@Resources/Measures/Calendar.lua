38bba9fc409446ec37e3df7e65cf917d350a8bad
John Murphy <smurfier20@gmail.com>
2012-11-05 11:25:00 -0600

    Calendar: Large Update Both: Added Weekday highlighting Script: Method Updates Script: Put things where they belong Skin: Put backgrounds behind text

--- Skins/Enigma/@Resources/Measures/Calendar.lua
+++ Skins/Enigma/@Resources/Measures/Calendar.lua
@@ -1,20 +1,30 @@
 function Initialize()
-	sRange = SELF:GetOption('Range', 'month'):lower()
 	iStartOnMondays = SELF:GetNumberOption('StartOnMonday', 0) > 0
 	iLeadingZeroes = SELF:GetNumberOption('LeadingZeroes', 0) > 0
 	iEDaysColor ='StyleCalendarText'..(SELF:GetNumberOption('ExtraDays') > 0 and 'Extra' or 'Invisible')
 	
-	Error=false
-	
-	iRange = {month = 42, week = 7}
-	if not iRange[sRange] then sRange = 'month' end
-	tCurrMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+	local sRange =  SELF:GetOption('Range', 'month'):gsub('%s', ''):lower()
+	if not ('month|week'):find(sRange) then ErrMsg(nil, 'Invalid Range: %s', sRange) end
+	if sRange == 'week' then
+		Range = {
+			days = 7,
+			formula = function(input) return Date.day + ((input - 1) - Rotate(Date.wday - 1)) end,
+			week = function() return 1 end,
+		}
+	else
+		Range = {
+			days = 42,
+			formula = function(input) return input - iStartDay end;
+			week = function() return math.ceil((Date.day + iStartDay) / 7) end,
+		}
+	end
+
 	iDayOnLastUpdate = 0
 
 	local tLabels = Delim(SELF:GetOption('DayLabels'))
 	if #tLabels < 7 then tLabels = {'S', 'M', 'T', 'W', 'R', 'F', 'S'} end
 	for i = 1, 7 do
-		SKIN:Bang('!SetOption', 'Day'..i..'Label', 'Text', tLabels[iStartOnMondays and (i % 7 + 1) or i])
+		SKIN:Bang('!SetOption', ('Day%dLabel'):format(i), 'Text', tLabels[iStartOnMondays and (i % 7 + 1) or i])
 	end
 	
 	hFile = {}
@@ -26,34 +36,39 @@ function Initialize()
 			Files[k] = Folder .. v
 		end
 	end
-	for _ ,FileName in ipairs(Files) do -- For each event file.
-		local file=io.open(SKIN:MakePathAbsolute(FileName), 'r') -- Open file in read only.
+	for _ , FileName in ipairs(Files) do -- For each event file.
+		local file = io.open(SKIN:MakePathAbsolute(FileName), 'r') -- Open file in read only.
 		if file then -- File is open.
 			local open, content, close = file:read('*all'):gsub('<!%-%-.-%-%->', ''):match('^.-<([^>]+)>(.+)<([^>]+)>$')
 			file:close()
-			if open:lower():match('([^%s]+)') == 'eventfile' and close:lower() == '/eventfile' then
+			if open:lower():match('(%S+)') == 'eventfile' and close:lower() == '/eventfile' then
 				local eFile, eSet = Keys(open), {}
-				local default = {month='', day='', year=false, desc='', title=false, color='',}
-				local sw={ -- Define Event File tags
-					set = function(x) table.insert(eSet, Keys(x)) end,
-					['/set'] = function(x) table.remove(eSet) end,
-					event = function(x)
-						local Tmp, dSet, tbl = Keys(x), ParseTbl(eSet), {}
+				local default = {month = '', day = '', year = false, desc = '', title = false, color = '',}
+				for tag, line in content:gmatch('<([^%s>]+)([^>]*)>') do
+					local ntag = tag:lower()
+
+					if ntag == 'set' then
+						table.insert(eSet, Keys(line))
+					elseif ntag == '/set' then
+						table.remove(eSet)
+					elseif ntag == 'event' then
+						local Tmp, dSet, tbl = Keys(line), {}, {}
+						for _, column in ipairs(eSet) do
+							for key, value in pairs(column) do
+								dSet[key] = value
+							end
+						end
 						for key, value in pairs(default) do tbl[key] = Tmp[key] or dSet[key] or eFile[key] or value end
 						table.insert(hFile, tbl)
-					end,
-					default = function(x, y) ErrMsg(0, 'Invalid Event Tag:', y) end, -- Error
-				}
-				for line in content:gmatch('%b<>') do
-					local tag = line:match('^<([^%s>]+)')
-					local f = sw[tag:lower()] or sw.default
-					f(line, tag)
+					else
+						ErrMsg(nil, 'Invalid Event Tag <%s> in %s', tag, FileName)
+					end
 				end
 			else
-				ErrMsg(0, 'Invalid Event File:', FileName)
+				ErrMsg(nil, 'Invalid Event File: %s', FileName)
 			end
 		else -- File could not be opened.
-			ErrMsg(0, 'File Read Error:', FileName)
+			ErrMsg(nil, 'File Read Error: %s', FileName)
 		end
 	end
 end -- Initialize
@@ -62,68 +77,86 @@ function Update()
 	Date = os.date('*t')
 	if Date.day ~= iDayOnLastUpdate then
 		iDayOnLastUpdate = Date.day
-		tCurrMonth[2] = 28+((((Date.year % 4) == 0 and (Date.year % 100) ~= 0) or (Date.year % 400) == 0) and 1 or 0)
-		iStartDay = Rotate(tonumber(os.date('%w', os.time{year = Date.year, month = Date.month, day = 1})))
+		local tstart = os.time{day = 1, month = Date.month, year = Date.year, isdst = false,}
+		local nstart = os.time{day = 1, month = (Date.month % 12 + 1), year = (Date.year + (Date.month == 12 and 1 or 0)), isdst = false,}
+		mLength, pLength, iStartDay = (nstart - tstart) / 86400, tonumber(os.date('%d', tstart - 86400)), Rotate(tonumber(os.date('%w', tstart)))
 		
 		Events()
 		
 		----------------------------------------------
 		-- !SETOPTIONS
 		
-		local case = {
-			week = function(z) return Date.day + ((z - 1) - Rotate(Date.wday - 1)) end,
-			month = function(z) return z-iStartDay end,
-		}
-		for meter = 1, iRange[sRange]  do
-			local styles, event, color, day = {'StyleCalendarText'}, '', '', case[sRange](meter)
+		for wday = 1, 7 do -- Set Weekday Labels styles
+			local Styles = {'StyleCalendarLabelBackground'}
+			if wday == 1 then
+				table.insert(Styles, 'StyleCalendarLabelBackgroundFirst')
+			end
+			if Rotate(Date.wday - 1) == (wday - 1) then
+				table.insert(Styles, 'StyleCalendarLabelBackgroundCurrent')
+			end
+			SKIN:Bang('!SetOption', ('Day%dLabelBackground'):format(wday), 'MeterStyle', table.concat(Styles, '|'))
+		end
+
+		for meter = 1, Range.days  do
+			local styles, event, day, color = {'StyleCalendarText'}, '', Range.formula(meter)
 			if meter == 1 then
 				table.insert(styles, 'StyleCalendarTextFirst')
 			elseif (meter % 7) == 1 then
 				table.insert(styles, 'StyleCalendarNewWeek')
 			end
 			
-			if day > 0 and day <= tCurrMonth[Date.month] and Hol[day] then
+			if day > 0 and day <= mLength and Hol[day] then
 				table.insert(styles, 'StyleCalendarEvent')
 				event = table.concat(Hol[day]['text'], '\n')
-				color = eColor(Hol[day]['color'])
+				-- Compress multiple custom colors
+				for _, value in ipairs(Hol[day].color) do
+					if value ~= '' then
+						if not color then
+							color = value
+						elseif color ~= value then
+							color = ''
+							break
+						end
+					end
+				end
 			end
 			
 			if day < 1 then -- Previous Month
-				day = day + tCurrMonth[Date.month == 1 and 12 or (Date.month - 1) ]
+				day = day + pLength
 				table.insert(styles, iEDaysColor)
-			elseif day > tCurrMonth[Date.month] then -- Next Month
-				day = day - tCurrMonth[Date.month]
+			elseif day > mLength then -- Next Month
+				day = day - mLength
 				table.insert(styles, iEDaysColor)
 			elseif Date.day == a then -- Today
 				table.insert(styles, 'StyleCalendarIndicatorText')
 			end
 			
-			for k,v in pairs{
+			for k,v in pairs{ -- Meter Properties
 				MeterStyle = table.concat(styles, '|'),
-				Text = iLeadingZeroes and string.format('%02d', day) or day,
+				Text = iLeadingZeroes and ('%02d'):format(day) or day,
 				ToolTipText = event,
-				SolidColor = color
+				SolidColor = color or '',
 			} do SKIN:Bang('!SetOption', 'Day' .. meter, k, v) end
 		end
-		if sRange == 'month' then
-			SKIN:Bang('!SetVariable', 'ThisWeek', math.ceil((Date.day + iStartDay) / 7))
-		end
-		SKIN:Bang('!SetVariable', 'Week', Rotate(Date.wday - 1))
+		for k, v in pairs{ -- Skin Variables
+			ThisWeek = Range.week(),
+			Week = Rotate(Date.wday - 1),
+		} do SKIN:Bang('!SetVariable', k, v) end
 	end
-	return Error and 'Error!' or 'Success!'
+	return rMessage or 'Success!'
 end -- Update
 
 function Events() -- Parse Events table.
 	Hol={}
 	
 	for _,event in ipairs(hFile) do
-		if SKIN:ParseFormula('(' .. Vars(event.month, event.desc) .. ')') == Date.month or event.month == '*' then
+		if SKIN:ParseFormula(('(%s)'):format(Vars(event.month, event.desc))) == Date.month or event.month == '*' then
 			
-			local day = SKIN:ParseFormula('(' .. Vars(event.day, event.desc) .. ')') or ErrMsg(0, 'Invalid Event Day', event.day, 'in', event.desc)
+			local day = SKIN:ParseFormula(('(%s)'):format(Vars(event.day, event.desc))) or ErrMsg(0, 'Invalid Event Day %s in %s', event.day, event.desc)
 			local color = event.color:match(',') and ConvertToHex(event.color) or event.color
 			local desc = table.concat{
 				event.desc,
-				event.year and ' (' .. math.abs(Date.year - event.year) .. ')' or '',
+				event.year and (' (%s)'):format(math.abs(Date.year - event.year)) or '',
 				event.title and ' -' .. event.title or '',
 			}
 			
@@ -137,24 +170,6 @@ function Events() -- Parse Events table.
 	end
 end -- Events
 
-function eColor(tbl) -- Makes allowance for multiple custom colors.
-	local color
-	-- Remove Empty Colors
-	for k, v in ipairs(tbl) do if v == '' then table.remove(tbl, k) end end
-	
-	for _,value in ipairs(tbl) do
-		if color then
-			if color ~= value then
-				return ''
-			end
-		else
-			color = value
-		end
-	end
-	
-	return color
-end -- eColor
-
 function Easter() -- Returns a timestamp representing easter of the current year.
 	local a, b, c, h, L, m = (Date.year % 19), math.floor(Date.year / 100), (Date.year % 100), 0, 0, 0
 	local d, e, f, i, k = math.floor(b / 4), (b % 4), math.floor((b + 8) / 25), math.floor(c / 4), (c % 4)
@@ -165,12 +180,28 @@ function Easter() -- Returns a timestamp representing easter of the current year
 	return os.time{month = math.floor((h + L - 7 * m + 114) / 31), day = ((h + L - 7 * m + 114) % 31 + 1), year = Date.year}
 end -- Easter
 
-function BuiltInEvents(default) -- Makes allowance for events that require complex calculations.
-	local tbl = default or {}
+function Vars(line, source) -- Makes allowance for {Variables}
+	local tbl = setmetatable({year = Date.year, today = Date.day, month = Date.month},
+		{ __index = function(_, var)
+			local D, W = {sun=0, mon=1, tue=2, wed=3, thu=4, fri=5, sat=6}, {first=0, second=1, third=2, fourth=3, last=4}
+			local v1,v2 = var:match('(.+)(...)')
+			if W[v1 or ''] and D[v2 or ''] then -- Variable day.
+				if v1 == 'last' then
+					local L = (36 + D[v2] - iStartDay)
+					return L - math.ceil((L - mLength) / 7) * 7
+				else
+					return Rotate(D[v2]) + 1 - iStartDay + (iStartDay > Rotate(D[v2]) and 7 or 0) + 7 * W[v1]
+				end
+			else -- Error
+				return ErrMsg(0, 'Invalid Variable {%s} in %s', var, source)
+			end
+		end})
+	
+	-- Built In Events
 	local AddVar = function(name, timestamp)
 		local temp = os.date('*t', timestamp)
-		tbl[name..'month'] = temp.month
-		tbl[name..'day'] = temp.day
+		tbl[name:lower() .. 'month'] = temp.month
+		tbl[name:lower() .. 'day'] = temp.day
 	end
 	-- Define {variables} here.
 	local sEaster = Easter()
@@ -178,32 +209,15 @@ function BuiltInEvents(default) -- Makes allowance for events that require compl
 	AddVar('goodfriday', (sEaster - 2 * 86400))
 	AddVar('ashwednesday', (sEaster - 46 * 86400))
 	AddVar('mardigras', (sEaster - 47 * 86400))
-	
-	return tbl
-end -- BuiltInEvents
 
-function Vars(line,source) -- Makes allowance for {Variables}
-	local D, W = {sun=0, mon=1, tue=2, wed=3, thu=4, fri=5, sat=6}, {first=0, second=1, third=2, fourth=3, last=4}
-	local tbl = BuiltInEvents{year = Date.year, today = Date.day, month = Date.month}
-	
-	return tostring(line):gsub('{([^}]+)}', function(variable)
-		local strip = variable:lower()
-		local v1,v2 = strip:match('(.+)(...)')
-		if tbl[strip] then
-			return tbl[strip]
-		elseif W[v1 or ''] and D[v2 or ''] then -- Variable day.
-			local L,wD = (36 + D[v2] - iStartDay), Rotate(D[v2])
-			return W[v1] < 4 and (wD + 1 - iStartDay + (iStartDay > wD and 7 or 0) + 7 * W[v1]) or (L - math.ceil((L - tCurrMonth[Date.month]) / 7) * 7)
-		else -- Error
-			return ErrMsg(0, 'Invalid Variable', variable, source and 'in '..source or '')
-		end
-	end)
+	return tostring(line):gsub('{([^}]+)}', function(variable) return tbl[variable:gsub('%s', ''):lower()] end)
 end -- Vars
 
 function ErrMsg(...) -- Used to display errors
-	Error=true
-	print(table.concat(arg, ' ', 2))
-	return arg[1]
+	local value = table.remove(arg, 1)
+	rMessage = string.format(unpack(arg))
+	print('Calendar: ' .. rMessage)
+	return value
 end -- ErrMsg
 
 function Rotate(value) return iStartOnMondays and ((value - 1 + 7) % 7) or value end
@@ -236,15 +250,3 @@ function ConvertToHex(color) -- Converts RGB colors to HEX
 	
 	return table.concat(hex)
 end -- ConvertToHex
-
-function ParseTbl(input) -- Compresses matrix into a single table.
-	local tbl = {}
-	
-	for _,column in ipairs(input) do
-		for key,value in pairs(column) do
-			tbl[key] = value
-		end
-	end
-	
-	return tbl
-end -- ParseTbl
