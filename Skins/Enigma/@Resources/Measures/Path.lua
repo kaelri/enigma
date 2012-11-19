26ef595547138ed38c125cd70d0bbda4c0dcd5c2
John Murphy <smurfier20@gmail.com>
2012-11-18 21:02:21 -0600

    Path: Updated code methods

--- Skins/Enigma/@Resources/Measures/Path.lua
+++ Skins/Enigma/@Resources/Measures/Path.lua
@@ -1,22 +1,25 @@
 function Initialize()
 	sVariablePrefix = SELF:GetOption('VariablePrefix')
 	sInput = SELF:GetOption('Input')
-	sInputType = string.lower(SELF:GetOption('InputType'))
-	sOutput = string.lower(SELF:GetOption('Output'))
+	sInputType = SELF:GetOption('InputType'):lower()
+	sOutput = SELF:GetOption('Output'):lower()
 end
 
 function Update()
-	local fCase={
-		variable=function() return SKIN:GetVariable(sInput) end,
-		measure=function() local msInput=SKIN:GetMeasure(sInput) return msInput:GetStringValue() end,
-		default=function() return sInput end,
-	}
-	local f=fCase[sInputType] or fCase.default
-	local text=f()
-	if text~='' then
-		local sDir, sName, sExt = string.match(text, '(.-)([^\\]-)%.([^%.]+)$')
-		local oCase={dir=sDir,name=sName,nameext=sName..'.'..sExt,ext=sExt}
-		for k,v in pairs(oCase) do SKIN:Bang('!SetVariable',sVariablePrefix..k,v) end
+	local text
+
+	if sInputType == 'variable' then
+		text = SKIN:GetVariable(sInput)
+	elseif sInputType == 'measure' then
+		text = SKIN:GetMeasure(sInput):GetStringValue()
+	else
+		text = sInput
+	end
+
+	if text ~= '' then
+		local sDir, sName, sExt = text:match('(.-)([^\\/]-)%.([^%.]+)$')
+		local oCase = {dir = sDir, name = sName, nameext = sName .. '.' .. sExt, ext = sExt}
+		for k, v in pairs(oCase) do SKIN:Bang('!SetVariable', sVariablePrefix .. k, v) end
 		return oCase[sOutput] or sName
 	else
 		return ''
