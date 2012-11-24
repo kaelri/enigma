f2d8083ed1ea5d040bbc841ea71f65fb67d67cd9
John Murphy <smurfier20@gmail.com>
2012-11-24 10:19:15 -0600

    Options: Last tweaks Collapsed single table lines to save space

--- Skins/Enigma/Options/Options.lua
+++ Skins/Enigma/Options/Options.lua
@@ -1,8 +1,4 @@
 function Initialize()
-	-- GET ROOT CONFIG
-	-- Because Enigma is fully portable, the root config name is not known.
-	--RootConfig = SKIN:GetVariable('CURRENTCONFIG'):match('(.-\\)')
-	
 	-- SET LABELS FOR CURRENT TAB
 	for Name in SELF:GetOption('LabelsQueue'):gmatch('[^%|]+') do
 		local pos = TablePosition(Options[Name].Loop, Name)
@@ -26,10 +22,12 @@ function Write(Key, Value, Wait)
 	-- WAIT OR REFRESH
 	if not Wait then
 		-- if Options[Key].Configs then
+		-- 	-- Because Enigma is fully portable, the root config name is not known.
+		-- 	local RootConfig = SKIN:GetVariable('CURRENTCONFIG'):match('(.-\\)')
 		-- 	for _, Config in ipairs(Options[Key].Configs) do
 		-- 		SKIN:Bang('!Refresh', RootConfig .. Config)
 		-- 	end
-		-- 	SKIN:Bang('!Refresh')
+		--  SKIN:Bang('!Refresh')
 		-- else
 		-- 	SKIN:Bang('!Refresh *')
 		-- end
@@ -83,8 +81,7 @@ function ParseGmail(_, Value)
 end
 
 function ParseGcal(_, Value)
-	Value = ParseProtocol(_, Value)
-	return Value:gsub('/basic', '/full')
+	return ParseProtocol(_, Value):gsub('/basic', '/full')
 end
 
 function ParseAppPath(Key, Value)
@@ -98,40 +95,20 @@ end
 
 Options = {
 	-- GENERAL
-	Note1Path = {
-		Configs = { 'Sidebar\\Notes', 'Sidebar\\Notes\\Notes1' }
-	},
-	Note2Path = {
-		Configs = { 'Sidebar\\Notes', 'Sidebar\\Notes\\Notes2' }
-	},
-	Note3Path = {
-		Configs = { 'Sidebar\\Notes', 'Sidebar\\Notes\\Notes3' }
-	},
-	NoteHeight = {
-		Configs = { 'Sidebar\\Notes', 'Sidebar\\Notes\\Notes1', 'Sidebar\\Notes\\Notes2', 'Sidebar\\Notes\\Notes3' }
-	},
-	Drive1 = {
-		Configs = { 'Sidebar\\System', 'Taskbar\\System', 'Taskbar\\System\\Drive\\Drive1' }
-	},
-	Drive2 = {
-		Configs = { 'Sidebar\\System', 'Taskbar\\System', 'Taskbar\\System\\Drive\\Drive2' }
-	},
-	Drive3 = {
-		Configs = { 'Sidebar\\System', 'Taskbar\\System', 'Taskbar\\System\\Drive\\Drive3' }
-	},
-	NetworkMaxDownload = {
-		Configs = { 'Sidebar\\Network', 'Taskbar\\Network', 'Taskbar\\Network\\Download' }
-	},
-	NetworkMaxUpload = {
-		Configs = { 'Sidebar\\Network', 'Taskbar\\Network', 'Taskbar\\Network\\Upload' }
-	},
+	Note1Path = {Configs = { 'Sidebar\\Notes', 'Sidebar\\Notes\\Notes1' },},
+	Note2Path = {Configs = { 'Sidebar\\Notes', 'Sidebar\\Notes\\Notes2' },},
+	Note3Path = {Configs = { 'Sidebar\\Notes', 'Sidebar\\Notes\\Notes3' },},
+	NoteHeight = {Configs = { 'Sidebar\\Notes', 'Sidebar\\Notes\\Notes1', 'Sidebar\\Notes\\Notes2', 'Sidebar\\Notes\\Notes3' },},
+	Drive1 = {Configs = { 'Sidebar\\System', 'Taskbar\\System', 'Taskbar\\System\\Drive\\Drive1' },},
+	Drive2 = {Configs = { 'Sidebar\\System', 'Taskbar\\System', 'Taskbar\\System\\Drive\\Drive2' },},
+	Drive3 = {Configs = { 'Sidebar\\System', 'Taskbar\\System', 'Taskbar\\System\\Drive\\Drive3' },},
+	NetworkMaxDownload = {Configs = { 'Sidebar\\Network', 'Taskbar\\Network', 'Taskbar\\Network\\Download' },},
+	NetworkMaxUpload = {Configs = { 'Sidebar\\Network', 'Taskbar\\Network', 'Taskbar\\Network\\Upload' },},
 	SystemGraphType = {
 		Configs = { 'Sidebar\\Network', 'Sidebar\\System' },
 		Loop    = { 'Floating', 'Percent' }
 	},
-	CalendarEventFile = {
-		Configs = { 'Sidebar\\Calendar' }
-	},
+	CalendarEventFile = {Configs = { 'Sidebar\\Calendar' },},
 	CalendarShowEvents = {
 		Configs = { 'Sidebar\\Calendar' },
 		Loop    = { '0', '1' },
@@ -152,39 +129,27 @@ Options = {
 		Loop    = { '0', '1' },
 		Labels  = { 'Sunday', 'Monday' }
 	},
-	GalleryPath = {
-		Configs = { 'Sidebar\\Gallery' }
-	},
+	GalleryPath = {Configs = { 'Sidebar\\Gallery' },},
 	GallerySubfolders = { 
 		Configs = { 'Sidebar\\Gallery' },
 		Loop    = { '0', '1' },
 		Labels  = { 'No', 'Yes' }
 	},
-	GalleryRatio = {
-		Configs = { 'Sidebar\\Gallery' }
-	},
+	GalleryRatio = {Configs = { 'Sidebar\\Gallery' },},
 	GalleryPosition = { 
 		Configs = { 'Sidebar\\Gallery' },
 		Loop    = { 'Center', 'Tile', 'Stretch', 'Fit', 'Fill' }
 	},
-	VolumeIncrement = {
-		Configs = { 'Sidebar\\Volume', 'Taskbar\\Volume' }
-	},
-	TriptychInterval = {
-		Configs = { 'Sidebar\\Gallery', 'Sidebar\\Notes', 'Sidebar\\Reader', 'Sidebar\\Reader\\Gcal', 'Sidebar\\Reader\\RememberTheMilk' }
-	},
+	VolumeIncrement = {Configs = { 'Sidebar\\Volume', 'Taskbar\\Volume' },},
+	TriptychInterval = {Configs = { 'Sidebar\\Gallery', 'Sidebar\\Notes', 'Sidebar\\Reader', 'Sidebar\\Reader\\Gcal', 'Sidebar\\Reader\\RememberTheMilk' },},
 	TriptychDisabled = { 
 		Configs = { 'Sidebar\\Gallery', 'Sidebar\\Notes', 'Sidebar\\Reader', 'Sidebar\\Reader\\Gcal', 'Sidebar\\Reader\\RememberTheMilk' },
 		Loop    = { '0', '1' },
 		Labels  = { 'Yes', 'No' }
 	},
-	ProcessInterval = {
-		Configs = { 'Sidebar\\Process' }
-	},
+	ProcessInterval = {Configs = { 'Sidebar\\Process' },},
 	-- MUSIC
-	MusicPlayerType = {
-		Configs = { 'Sidebar\\Music', 'Taskbar\\Music' }
-	},
+	MusicPlayerType = {Configs = { 'Sidebar\\Music', 'Taskbar\\Music' },},
 	MusicPlayer = {},
 	--FEEDS
 	Feed1 = {
@@ -204,9 +169,7 @@ Options = {
 		Parse   = ParseGmail,
 		Dependents = {'GmailPassword', 'GmailUrl', 'GmailDomain'}
 	},
-	GmailPassword = {
-		Configs = { 'Sidebar\\Reader\\Gmail', 'Taskbar\\Reader\\Gmail' },
-	},
+	GmailPassword = {Configs = { 'Sidebar\\Reader\\Gmail', 'Taskbar\\Reader\\Gmail' },},
 	GmailUrl = {},
 	GmailDomain = {},
 	FacebookFeed = {
@@ -230,21 +193,11 @@ Options = {
 		Loop    = { '0', '1' },
 		Labels  = { 'No', 'Yes' }
 	},
-	RTMusername = {
-		Configs = { 'Sidebar\\Reader\\RememberTheMilk', 'Taskbar\\Reader\\RememberTheMilk' }
-	},
-	RTMpassword = {
-		Configs = { 'Sidebar\\Reader\\RememberTheMilk', 'Taskbar\\Reader\\RememberTheMilk' }
-	},
-	RTMlist1 = {
-		Configs = { 'Sidebar\\Reader\\RememberTheMilk', 'Taskbar\\Reader\\RememberTheMilk' }
-	},
-	RTMlist2 = {
-		Configs = { 'Sidebar\\Reader\\RememberTheMilk' }
-	},
-	RTMlist3 = {
-		Configs = { 'Sidebar\\Reader\\RememberTheMilk' }
-	},
+	RTMusername = {Configs = { 'Sidebar\\Reader\\RememberTheMilk', 'Taskbar\\Reader\\RememberTheMilk' },},
+	RTMpassword = {Configs = { 'Sidebar\\Reader\\RememberTheMilk', 'Taskbar\\Reader\\RememberTheMilk' },},
+	RTMlist1 = {Configs = { 'Sidebar\\Reader\\RememberTheMilk', 'Taskbar\\Reader\\RememberTheMilk' },},
+	RTMlist2 = {Configs = { 'Sidebar\\Reader\\RememberTheMilk' },},
+	RTMlist3 = {Configs = { 'Sidebar\\Reader\\RememberTheMilk' },},
 	-- WORLD
 	WeatherCode = {
 		Dependents = { 'WeatherCodeName', 'WeatherCodeLat', 'WeatherCodeLon' },
@@ -308,178 +261,102 @@ Options = {
 	World3WeatherCodeLat = {},
 	World3WeatherCodeLon = {},
 	-- APPS
-	App1 = {
-		Configs = { 'Taskbar\\Launcher\\Launcher1' }
-	},
+	App1 = {Configs = { 'Taskbar\\Launcher\\Launcher1' },},
 	App1Path = {
-			Dependents = { 'App1PathHandle' },
+		Dependents = { 'App1PathHandle' },
 		Configs    = { 'Taskbar\\Launcher\\Launcher1' },
 		Parse      = ParseAppPath
 	},
 	App1PathHandle = {},
-	App1Label = {
-		Configs = { 'Taskbar\\Launcher\\Launcher1' }
-	},
-	App1Icon = {
-		Configs = { 'Taskbar\\Launcher\\Launcher1' }
-	},
-	App2 = {
-		Configs = { 'Taskbar\\Launcher\\Launcher2' }
-	},
+	App1Label = {Configs = { 'Taskbar\\Launcher\\Launcher1' },},
+	App1Icon = {Configs = { 'Taskbar\\Launcher\\Launcher1' },},
+	App2 = {Configs = { 'Taskbar\\Launcher\\Launcher2' },},
 	App2Path = {
 		Dependents = { 'App2PathHandle' },
 		Configs    = { 'Taskbar\\Launcher\\Launcher2' },
 		Parse      = ParseAppPath
 	},
 	App2PathHandle = {},
-	App2Label = {
-		Configs = { 'Taskbar\\Launcher\\Launcher2' }
-	},
-	App2Icon = {
-		Configs = { 'Taskbar\\Launcher\\Launcher2' }
-	},
-	App3 = {
-		Configs = { 'Taskbar\\Launcher\\Launcher3' }
-	},
+	App2Label = {Configs = { 'Taskbar\\Launcher\\Launcher2' },},
+	App2Icon = {Configs = { 'Taskbar\\Launcher\\Launcher2' },},
+	App3 = {Configs = { 'Taskbar\\Launcher\\Launcher3' },},
 	App3Path = {
 		Dependents = { 'App3PathHandle' },
 		Configs    = { 'Taskbar\\Launcher\\Launcher3' },
 		Parse      = ParseAppPath
 	},
 	App3PathHandle = {},
-	App3Label = {
-		Configs = { 'Taskbar\\Launcher\\Launcher3' }
-	},
-	App3Icon = {
-		Configs = { 'Taskbar\\Launcher\\Launcher3' }
-	},
-	App4 = {
-		Configs = { 'Taskbar\\Launcher\\Launcher4' }
-	},
+	App3Label = {Configs = { 'Taskbar\\Launcher\\Launcher3' },},
+	App3Icon = {Configs = { 'Taskbar\\Launcher\\Launcher3' },},
+	App4 = {Configs = { 'Taskbar\\Launcher\\Launcher4' },},
 	App4Path = {
 		Dependents = { 'App4PathHandle' },
 		Configs    = { 'Taskbar\\Launcher\\Launcher4' },
 		Parse      = ParseAppPath
 	},
 	App4PathHandle = {},
-	App4Label = {
-		Configs = { 'Taskbar\\Launcher\\Launcher4' }
-	},
-	App4Icon = {
-		Configs = { 'Taskbar\\Launcher\\Launcher4' }
-	},
-	App5 = {
-		Configs = { 'Taskbar\\Launcher\\Launcher5' }
-	},
+	App4Label = {Configs = { 'Taskbar\\Launcher\\Launcher4' },},
+	App4Icon = {Configs = { 'Taskbar\\Launcher\\Launcher4' },},
+	App5 = {Configs = { 'Taskbar\\Launcher\\Launcher5' },},
 	App5Path = {
 		Dependents = { 'App5PathHandle' },
 		Configs    = { 'Taskbar\\Launcher\\Launcher5' },
 		Parse      = ParseAppPath
 	},
 	App5PathHandle = {},
-	App5Label = {
-		Configs = { 'Taskbar\\Launcher\\Launcher5' }
-	},
-	App5Icon = {
-		Configs = { 'Taskbar\\Launcher\\Launcher5' }
-	},
+	App5Label = {Configs = { 'Taskbar\\Launcher\\Launcher5' },},
+	App5Icon = {Configs = { 'Taskbar\\Launcher\\Launcher5' },},
 	-- SEARCH
-	Search1 = {
-		Configs = { 'Taskbar\\Search\\Search1', 'Taskbar\\Search\\Run' }
-	},
+	Search1 = {Configs = { 'Taskbar\\Search\\Search1', 'Taskbar\\Search\\Run' },},
 	Search1Command = {
 		Configs = { 'Taskbar\\Search\\Search1', 'Taskbar\\Search\\Run' },
 		Parse   = ParseProtocol
 	},
-	Search1Icon = {
-		Configs = { 'Taskbar\\Search\\Search1', 'Taskbar\\Search\\Run' }
-	},
-	Search2 = {
-		Configs = { 'Taskbar\\Search\\Search2', 'Taskbar\\Search\\Run' }
-	},
+	Search1Icon = {Configs = { 'Taskbar\\Search\\Search1', 'Taskbar\\Search\\Run' },},
+	Search2 = {Configs = { 'Taskbar\\Search\\Search2', 'Taskbar\\Search\\Run' },},
 	Search2Command = {
 		Configs = { 'Taskbar\\Search\\Search2', 'Taskbar\\Search\\Run' },
 		Parse   = ParseProtocol
 	},
-	Search2Icon = {
-		Configs = { 'Taskbar\\Search\\Search2', 'Taskbar\\Search\\Run' }
-	},
-	Search3 = {
-		Configs = { 'Taskbar\\Search\\Search3', 'Taskbar\\Search\\Run' }
-	},
+	Search2Icon = {Configs = { 'Taskbar\\Search\\Search2', 'Taskbar\\Search\\Run' },},
+	Search3 = {Configs = { 'Taskbar\\Search\\Search3', 'Taskbar\\Search\\Run' },},
 	Search3Command = {
 		Configs = { 'Taskbar\\Search\\Search3', 'Taskbar\\Search\\Run' },
 		Parse   = ParseProtocol
 	},
-	Search3Icon = {
-		Configs = { 'Taskbar\\Search\\Search3', 'Taskbar\\Search\\Run' }
-	},
-	Search4 = {
-		Configs = { 'Taskbar\\Search\\Search4', 'Taskbar\\Search\\Run' }
-	},
+	Search3Icon = {Configs = { 'Taskbar\\Search\\Search3', 'Taskbar\\Search\\Run' },},
+	Search4 = {Configs = { 'Taskbar\\Search\\Search4', 'Taskbar\\Search\\Run' },},
 	Search4Command = {
 		Configs = { 'Taskbar\\Search\\Search4', 'Taskbar\\Search\\Run' },
 		Parse   = ParseProtocol
 	},
-	Search4Icon = {
-		Configs = { 'Taskbar\\Search\\Search4', 'Taskbar\\Search\\Run' }
-	},
-	Search5 = {
-		Configs = { 'Taskbar\\Search\\Search5', 'Taskbar\\Search\\Run' }
-	},
+	Search4Icon = {Configs = { 'Taskbar\\Search\\Search4', 'Taskbar\\Search\\Run' },},
+	Search5 = {Configs = { 'Taskbar\\Search\\Search5', 'Taskbar\\Search\\Run' },},
 	Search5Command = {
 		Configs = { 'Taskbar\\Search\\Search5', 'Taskbar\\Search\\Run' },
 		Parse   = ParseProtocol
 	},
-	Search5Icon = {
-		Configs = { 'Taskbar\\Search\\Search5', 'Taskbar\\Search\\Run' }
-	},
+	Search5Icon = {Configs = { 'Taskbar\\Search\\Search5', 'Taskbar\\Search\\Run' },},
 	-- FORMAT
 	Stylesheet = {},
-	Color1 = {
-		File = true
-	},
-	ColorLink = {
-		File = true
-	},
-	ColorBorder = {
-		File = true
-	},
-	ColorEvent = {
-		File = true
-	},
-	ColorPanel = {
-		File = true
-	},
-	ColorFilter = {
-		File = true
-	},
-	ColorTransparent = {
-		File = true
-	},
+	Color1 = {File = true},
+	ColorLink = {File = true},
+	ColorBorder = {File = true},
+	ColorEvent = {File = true},
+	ColorPanel = {File = true},
+	ColorFilter = {File = true},
+	ColorTransparent = {File = true},
 	ColorImage = {
 		File = true,
 		Loop = { 'W', 'B' },
 		Labels = { 'W', 'B' }
 	},
-	Size1 = {
-		File = true
-	},
-	Size4 = {
-		File = true
-	},
-	Size3 = {
-		File = true
-	},
-	Size2 = {
-		File = true
-	},
-	Font = {
-		File = true
-	},
-	SkinBackgroundAlpha  = {
-		File = true
-	},
+	Size1 = {File = true},
+	Size4 = {File = true},
+	Size3 = {File = true},
+	Size2 = {File = true},
+	Font = {File = true},
+	SkinBackgroundAlpha  = {File = true},
 	HideBordersTop = {
 		File = true, 
 		Loop = { '0', '1' },
@@ -491,9 +368,7 @@ Options = {
 		Labels = { 'Yes', 'No' }
 	},
 	--LAYOUT
-	SidebarWidth = {
-		File = true
-	},
+	SidebarWidth = {File = true},
 	SidebarSpacingFixed = {
 		File = true,
 		Configs = { 'Sidebar', 'Sidebar\\Sidebar2' }, 
@@ -512,9 +387,7 @@ Options = {
 		File = true,
 		Configs = { 'Sidebar', 'Sidebar\\Sidebar2' }
 	},
-	TaskbarHeight = {
-		File = true
-	},
+	TaskbarHeight = {File = true},
 	TaskbarSpacingFixed = {
 		File = true, 
 		Configs = { 'Taskbar', 'Taskbar\\Taskbar2' },
@@ -533,12 +406,8 @@ Options = {
 		File = true,
 		Configs = { 'Taskbar', 'Taskbar\\Taskbar2' },
 	},
-	TaskbarMaxSkinWidth  = {
-		File = true
-	},
-	TaskbarMinSkinWidth  = {
-		File = true
-	},
+	TaskbarMaxSkinWidth  = {File = true},
+	TaskbarMinSkinWidth  = {File = true},
 	TaskbarHideTriptych = {
 		File = true,
 		Configs = { 'Taskbar', 'Taskbar\\Taskbar2' },
