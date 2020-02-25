9d2aa62c734a2d2be386af377e7e4f93bf5fc482
John Murphy <smurfier@outlook.com>
2020-02-24 22:22:07 -0600

    Run: Make it easier to add more commands

--- Skins/Enigma/@Resources/Measures/Run.lua
+++ Skins/Enigma/@Resources/Measures/Run.lua
@@ -14,25 +14,52 @@ end
 
 function Run(command)
 	lcommand = command:lower()
-	if lcommand:match('^search%d? .+') then
-		local num, term = command:match('^......(%d?) (.+)')
-		num = tonumber(num ~= '' and num or 1)
-		local searchnum = ('Search%dCommand'):format((num >= 1 and num <= 5) and num or 1)
-		local search = SKIN:GetVariable(searchnum):gsub('%$UserInput%$', (term:gsub('%s', '%%%%20')))
-		SKIN:Bang(search)
-	elseif lcommand:match('^web .+') then
-		local tbl, term = {}, command:match('^... (.+)')
-		for word in term:gmatch('[^%.]+') do table.insert(tbl, word) end
-		SKIN:Bang('"http://' .. (#tbl >= 3 and '' or 'www.') .. table.concat(tbl, '.') .. (#tbl >= 2 and '"' or '.com"'))
-	elseif lcommand:match('^https?://.+') then
-		SKIN:Bang(('%q'):format(command))
-	elseif lcommand:match('^options') then
-		local term = lcommand:match('^.+ (.+)') or 'none'
-		if ('home|general|music|feeds|world|apps|search|format|layout'):find(term:gsub('|', '')) then
-			SKIN:Bang('!WriteKeyValue', 'Variables', 'Panel', term, '#ROOTCONFIGPATH#Options\\Options.ini')
+
+	commands = {
+		{
+			regex = '^search%d? .+',
+			command = function()
+				local num, term = command:match('^......(%d?) (.+)')
+				num = tonumber(num ~= '' and num or 1)
+				local searchnum = ('Search%dCommand'):format((num >= 1 and num <= 5) and num or 1)
+				local search = SKIN:GetVariable(searchnum):gsub('%$UserInput%$', (term:gsub('%s', '%%%%20')))
+				SKIN:Bang(search)
+			end
+		},
+		{
+			regex = '^web .+',
+			command = function()
+				local tbl, term = {}, command:match('^... (.+)')
+				for word in term:gmatch('[^%.]+') do
+					table.insert(tbl, word)
+				end
+				SKIN:Bang('"http://' .. (#tbl >= 3 and '' or 'www.') .. table.concat(tbl, '.') .. (#tbl >= 2 and '"' or '.com"'))
+			end
+		},
+		{
+			regex = '^https?://.+',
+			command = function()
+				SKIN:Bang(('%q'):format(command))
+			end
+		},
+		{
+			regex = '^options',
+			command = function()
+				local term = lcommand:match('^.+ (.+)') or 'none'
+				if ('home|general|music|feeds|world|apps|search|format|layout'):find(term:gsub('|', '')) then
+					SKIN:Bang('!WriteKeyValue', 'Variables', 'Panel', term, '#ROOTCONFIGPATH#Options\\Options.ini')
+				end
+				SKIN:Bang('!ActivateConfig', 'Enigma\\Options')
+			end
+		}
+	}
+
+	for _, this in pairs(commands) do
+		if (lcommand:match(this.regex)) then
+			this.command()
+			return false
 		end
-		SKIN:Bang('!ActivateConfig', 'Enigma\\Options')
-	else
-		SKIN:Bang(Execute[lcommand] or command)
 	end
+	-- Execute if no other commands matched
+	SKIN:Bang(Execute[lcommand] or command)
 end
